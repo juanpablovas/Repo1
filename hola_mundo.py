62edcd7f785b9b052957f4a0fe2964fd7ed52812
Juan Pablo Vasquez Muñoz <juanp.vasquezm@upb.edu.co>
2026-01-23 15:10:53 -0500

    Agrego codigo para calcular los numeros primos entre 1 y 1000

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,17 @@
 # mi_primer_script.py
 print("¡Hola, GitHub!")
+
+def primos_hasta(n):
+    es_primo = [True] * (n + 1)
+    es_primo[0] = es_primo[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if es_primo[i]:
+            for j in range(i * i, n + 1, i):
+                es_primo[j] = False
+
+    return [i for i in range(2, n + 1) if es_primo[i]]
+
+
+primos = primos_hasta(1000)
+print(primos)
